208a92fce2652bcf551ee0beff0c17b476b8415b
williamd4112 <williamd4112@hotmail.com>
2017-11-04 04:32:59 +0800

    add rl

---
+++ robot_project/predictor.py
@@ -0,0 +1,149 @@
+import numpy as np
+import os
+import sys
+import time
+import random
+import uuid
+import argparse
+import multiprocessing
+import threading
+
+import cv2
+import tensorflow as tf
+import six
+from six.moves import queue
+
+from tensorpack import *
+from tensorpack.utils.concurrency import *
+from tensorpack.utils.serialize import *
+from tensorpack.utils.stats import *
+from tensorpack.tfutils import symbolic_functions as symbf
+from tensorpack.tfutils.gradproc import MapGradient, SummaryGradient
+from tensorpack.utils.gpu import get_nr_gpu
+
+
+from tensorpack.RL import *
+
+from collections import deque
+if six.PY3:
+    from concurrent import futures
+    CancelledError = futures.CancelledError
+else:
+    CancelledError = Exception
+
+IMAGE_SIZE = (84, 84)
+# FRAME_HISTORY = 4
+FRAME_HISTORY = 1
+GAMMA = 0.99
+CHANNEL = FRAME_HISTORY * 3
+IMAGE_SHAPE3 = IMAGE_SIZE + (CHANNEL,)
+NUM_ACTIONS = 3
+
+class Model(ModelDesc):
+    def _get_inputs(self):
+        assert NUM_ACTIONS is not None
+        return [InputDesc(tf.uint8, (None,) + IMAGE_SHAPE3, 'state'),
+                InputDesc(tf.int64, (None,), 'action'),
+                InputDesc(tf.float32, (None,), 'futurereward'),
+                InputDesc(tf.float32, (None,), 'action_prob'),
+                ]
+
+    def _get_NN_prediction(self, image):
+        image = tf.cast(image, tf.float32) / 255.0
+        with argscope(Conv2D, nl=tf.nn.relu):
+            l = Conv2D('conv0', image, out_channel=32, kernel_shape=5)
+            l = MaxPooling('pool0', l, 2)
+            l = Conv2D('conv1', l, out_channel=32, kernel_shape=5)
+            l = MaxPooling('pool1', l, 2)
+            l = Conv2D('conv2', l, out_channel=64, kernel_shape=4)
+            l = MaxPooling('pool2', l, 2)
+            l = Conv2D('conv3', l, out_channel=64, kernel_shape=3)
+
+        l = FullyConnected('fc0', l, 512, nl=tf.identity)
+        l = PReLU('prelu', l)
+        logits = FullyConnected('fc-pi', l, out_dim=NUM_ACTIONS, nl=tf.identity)    # unnormalized policy
+        value = FullyConnected('fc-v', l, 1, nl=tf.identity)
+        return logits, value
+
+    def _build_graph(self, inputs):
+        state, action, futurereward, action_prob = inputs
+        logits, value = self._get_NN_prediction(state)
+        value = tf.squeeze(value, [1], name='pred_value')  # (B,)
+        policy = tf.nn.softmax(logits, name='policy')
+        is_training = get_current_tower_context().is_training
+        if not is_training:
+            return
+        log_probs = tf.log(policy + 1e-6)
+
+        log_pi_a_given_s = tf.reduce_sum(
+            log_probs * tf.one_hot(action, NUM_ACTIONS), 1)
+        advantage = tf.subtract(tf.stop_gradient(value), futurereward, name='advantage')
+
+        pi_a_given_s = tf.reduce_sum(policy * tf.one_hot(action, NUM_ACTIONS), 1)  # (B,)
+        importance = tf.stop_gradient(tf.clip_by_value(pi_a_given_s / (action_prob + 1e-8), 0, 10))
+
+        policy_loss = tf.reduce_sum(log_pi_a_given_s * advantage * importance, name='policy_loss')
+        xentropy_loss = tf.reduce_sum(policy * log_probs, name='xentropy_loss')
+        value_loss = tf.nn.l2_loss(value - futurereward, name='value_loss')
+
+        pred_reward = tf.reduce_mean(value, name='predict_reward')
+        advantage = symbf.rms(advantage, name='rms_advantage')
+        entropy_beta = tf.get_variable('entropy_beta', shape=[],
+                                       initializer=tf.constant_initializer(0.01), trainable=False)
+        self.cost = tf.add_n([policy_loss, xentropy_loss * entropy_beta, value_loss])
+        self.cost = tf.truediv(self.cost,
+                               tf.cast(tf.shape(futurereward)[0], tf.float32),
+                               name='cost')
+        summary.add_moving_summary(policy_loss, xentropy_loss,
+                                   value_loss, pred_reward, advantage,
+                                   self.cost, tf.reduce_mean(importance, name='importance'))
+
+    def _get_optimizer(self):
+        lr = symbf.get_scalar_var('learning_rate', 0.001, summary=True)
+        opt = tf.train.AdamOptimizer(lr, epsilon=1e-3)
+
+        gradprocs = [MapGradient(lambda grad: tf.clip_by_average_norm(grad, 0.1)),
+                     SummaryGradient()]
+        opt = optimizer.apply_grad_processors(opt, gradprocs)
+        return opt
+
+def reszieImage(state):
+    return np.reshape(cv2.resize(state, IMAGE_SIZE[::-1]),(IMAGE_SIZE + (int(CHANNEL/FRAME_HISTORY),)))
+
+counter = 0
+def getPredictor(load_path=None, transform=False):
+    assert load_path is not None
+    cfg = PredictConfig(
+            model=Model(),
+            session_init=get_model_loader(load_path),
+            input_names=['state'],
+            output_names=['policy'])
+    predfunc = OfflinePredictor(cfg)
+    q = deque()
+    def step(state):
+        if len(q)==0:
+            q.extend(np.repeat([state], FRAME_HISTORY, axis=0))
+        else:
+            q.append(state)
+            q.popleft()
+        obs_stack=list(q)
+        obs_stack = np.concatenate(obs_stack, axis=2)
+        act = predfunc([[obs_stack]])[0][0].argmax() 
+        act = act
+        if transform is True:
+            act = ACTION_TABLE[act]
+        return act
+    return step
+
+def getSingleFramePredictor(load_path=None, transform=False):
+    assert load_path is not None
+    cfg = PredictConfig(
+            model=Model(),
+            session_init=get_model_loader(load_path),
+            input_names=['state'],
+            output_names=['policy'])
+    predfunc = OfflinePredictor(cfg)
+    def step(x):
+        act = predfunc([[x]])[0][0].argmax()
+        return act
+    return step
